57d149005925b7f57c7dbdd748e5561116fbff3b
jwwliew <junweiliew7@gmail.com>
2022-11-01 01:46:32 +0800

    Not tested but, fixed issues:
    1) Fixed threading issues for command 3, red LED should now blink at 8 times per sec
    2) Removed threading for commands 0 and 1
    3) Added directions on how to exit program
    4) Edited some comments
    5) Other minor optimization

--- led_main.c
+++ led_main.c
@@ -1,11 +1,14 @@
+/* gcc test.c -l pigpio -l pthread
+   sudo ./a.out */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <pigpio.h>
 
-pthread_t threads[4];
-int toggleKillCommand[] = {1, 1, 1, 1}; // on/off toggle to send a signal for function to end
+pthread_t blink_both, red_eight, green_reduce, end_prog;
+int arrToggleKillThread[] = {1, 1, 1}; // used for signalling to threads to end
 int currentCommand = -1;
 int previousCommand = -1;
 
@@ -14,12 +17,8 @@ int RED_LED = 19;   // pin number that LED is connected to
 
 int getUserCommand();
 void recordPreviousCommand(int command);
-void killCommand(int command);
-
-void *offLedCaller(void *command);
-void *onLedCaller(void *command);
-void *blinkBothLedCaller(void *command);
-void *reduceBrightnessLedCaller(void *command);
+void killThread(int command);
+void *endProgram(void *arg);
 
 void initialize();
 void *on_leds();
@@ -33,119 +32,99 @@ int main()
     initialize(); // initialize LEDs
     do
     {
-        recordPreviousCommand(currentCommand);                                                        // keeps track of previous command
-        currentCommand = getUserCommand();                                                            // set user's input into variable "currentCommand"
-        if (currentCommand == 0 || currentCommand == 1 || currentCommand == 2 || currentCommand == 3) // if current input is a valid command, kill previous command
+        recordPreviousCommand(currentCommand); // keeps track of previous command
+        currentCommand = getUserCommand();     // set user's input into variable "currentCommand"
+
+        if (previousCommand == currentCommand) // check if previous command = current command. If same, loop repeats to ask for user input again
         {
-            killCommand(previousCommand);
-            pthread_join(threads[previousCommand], NULL); // waits for previous command's thread to terminate before executing new command
+            printf("\nAlready executing this command, please pick another command >:) \n");
+        }
+        else
+        {
+            if (previousCommand == 2)
+            {
+                killThread(previousCommand);
+            }
+            if (previousCommand == 3)
+            {
+                killThread(previousCommand);
+            }
+            switch (currentCommand) // creates a thread for the LED function. At the same time, still allows user's input for next instruction
+            {
+            case 0:
+                off_leds();
+                break;
+            case 1:
+                on_leds();
+                break;
+            case 2:
+                pthread_create(&blink_both, NULL, &blink_leds_2Hz, NULL);
+                break;
+            case 3:
+                pthread_create(&red_eight, NULL, &blink_red_8Hz, NULL);
+                pthread_create(&green_reduce, NULL, &blink_green_2Hz_reduced_brightness, NULL);
+                break;
+            case 1234:
+                pthread_create(&end_prog, NULL, &endProgram, NULL);
+                pthread_join(end_prog, NULL); // waits for the endProgram thread to finish before exiting the program
+                printf("\n Exiting program. . . . \n");
+                break;
+            default: // if user's input is not one of the listed commands
+                printf("\n Please try again and input 0,1,2 or 3.\n");
+                break;
+            }
         }
 
-        switch (currentCommand) // creates a thread for the LED function. At the same time, still allows user's input for next instruction
-        {
-        case 0:
-            pthread_create(&threads[currentCommand], NULL, &offLedCaller, &currentCommand);
-            break;
-        case 1:
-            pthread_create(&threads[currentCommand], NULL, &onLedCaller, &currentCommand);
-            break;
-        case 2:
-            pthread_create(&threads[currentCommand], NULL, &blinkBothLedCaller, &currentCommand);
-            break;
-        case 3:
-            pthread_create(&threads[currentCommand], NULL, &reduceBrightnessLedCaller, &currentCommand);
-            break;
-        default: // if user's input is not in the listed commands
-            printf("\n Please try again and input 0,1,2 or 3.\n");
-            break;
-        }
+    } while (currentCommand != 1234); // if user inputs 1234, program stops asking for user's input and exits program
 
-    } while (currentCommand != 1234); // if user inputs 1234, program stops asking for user's input and starts to end program
-
-    printf("\n Exiting program. . . . \n");
-    killCommand(previousCommand); // stops the last called command before ending program
     return 0;
 }
 
-void recordPreviousCommand(int command) // records the current command into previous command before taking in new user input
+void recordPreviousCommand(int command) // records the previous command for thread killing purposes and optimization
 {
-    if (command == 0 || command == 1 || command == 2 || command == 3) // only records command if current input is valid
+    if (command == 0 || command == 1 || command == 2 || command == 3) // only records command if current user input is valid
     {
         previousCommand = command;
     }
 }
 
-void killCommand(int command) // terminates the specified command
+void killThread(int command) // terminates the specified command
 {
-    toggleKillCommand[command] = 0; // sets the value in array of int to 0, acts as a toggle/signal for the LED functions to terminate it's while loop.
-                                    // function continulously checks for toggle on every loop, once toggle is 0, function stops looping
+    if (command == 2)
+    {
+        arrToggleKillThread[0] = 0;     // sets the value in array of int to 0, acts as a toggle/signal for the LED functions to terminate it's while loop.
+        pthread_join(blink_both, NULL); // waits for thread to end before continuing
+    }
+    else if (command == 3)
+    {
+        arrToggleKillThread[1] = 0;       // same as above, acts as toggle
+        arrToggleKillThread[2] = 0;       // same as above, acts as toggle
+        pthread_join(red_eight, NULL);    // waits for thread to end before continuing
+        pthread_join(green_reduce, NULL); // waits for thread to end before continuing
+    }
+}
+
+void *endProgram(void *arg) // function ensures all threads and leds are properly terminated/shut off properly
+{
+    printf("\n Terminating processes, please wait for the program to end :) \n");
+    if (previousCommand == 2 || previousCommand == 3) // if last command is 2 or 3, ensure thread(s) are properly terminated
+    {
+        killThread(previousCommand);
+    }
+    else if (previousCommand == 1) // if leds are on, turn it off
+    {
+        off_leds();
+    }
 }
 
 int getUserCommand() // gets user input from console
 {
     int user_input;
-    printf("\nSoftware commands:\n 0 to turn off both red and green LED lights \n 1 to turn on red and green LED lights \n 2 to keep blinking with the pattern of 2 times per second, for both red and green LED lights \n 3 to change the pattern of the red LED light and green LED light in different ways");
+    printf("\nSoftware commands:\n 0 to turn off both red and green LED lights \n 1 to turn on red and green LED lights \n 2 to keep blinking with the pattern of 2 times per second, for both red and green LED lights \n 3 to change the pattern of the red LED light and green LED light in different ways \n 1234 to exit the program");
     printf("\n \nPlease input software command:");
     scanf("%d", &user_input);
 
     return user_input;
-}
-
-void *offLedCaller(void *command)
-{
-    int index = *(int *)command; // cast command to int pointer and dereference
-    while (toggleKillCommand[index])
-    {
-        off_leds();
-        if (toggleKillCommand[index] == 0) // checks for toggle continiously in while loop, once killCommand() is called, exit loop
-        {
-            toggleKillCommand[index] = 1; // resets toggle
-            break;
-        }
-    }
-}
-
-void *onLedCaller(void *command)
-{
-    int index = *(int *)command; // cast command to int pointer and dereference
-    while (toggleKillCommand[index])
-    {
-        on_leds();
-        if (toggleKillCommand[index] == 0) // checks for toggle continiously in while loop, once killCommand() is called, exit loop
-        {
-            toggleKillCommand[index] = 1; // resets toggle
-            break;
-        }
-    }
-}
-
-void *blinkBothLedCaller(void *command)
-{
-    int index = *(int *)command; // cast command to int pointer and dereference
-    while (toggleKillCommand[index])
-    {
-        blink_leds_2Hz();
-        if (toggleKillCommand[index] == 0) // checks for toggle continiously in while loop, once killCommand() is called, exit loop
-        {
-            toggleKillCommand[index] = 1; // resets toggle
-            break;
-        }
-    }
-}
-
-void *reduceBrightnessLedCaller(void *command)
-{
-    int index = *(int *)command; // cast command to int pointer and dereference
-    while (toggleKillCommand[index])
-    {
-        blink_red_8Hz();
-        blink_green_2Hz_reduced_brightness();
-        if (toggleKillCommand[index] == 0) // checks for toggle continiously in while loop, once killCommand() is called, exit loop
-        {
-            toggleKillCommand[index] = 1; // resets toggle
-            break;
-        }
-    }
 }
 
 void initialize()
@@ -164,36 +143,68 @@ void initialize()
 
 void *on_leds()
 {
+    printf("LEDS ON");
     gpioWrite(GREEN_LED, 1);
     gpioWrite(RED_LED, 1);
 }
 
 void *off_leds()
 {
+    printf("LEDS OFF");
     gpioWrite(GREEN_LED, 0);
     gpioWrite(RED_LED, 0);
 }
 
 void *blink_leds_2Hz()
 {
-    on_leds();
-    usleep(250000);
-    off_leds();
-    usleep(250000);
+    while (arrToggleKillThread[0])
+    {
+        printf("BOTH LED BLINKING AT SAME RATE \n");
+        on_leds();
+        usleep(250000);
+        off_leds();
+        usleep(250000);
+        if (arrToggleKillThread[0] == 0) // checks for toggle continiously in while loop, once killThread() is called, exit loop.
+                                         // Ensures thread properly terminates itself with no memory leaks
+        {
+            arrToggleKillThread[0] = 1; // resets toggle
+            break;
+        }
+    }
 }
 
 void *blink_red_8Hz()
 {
-    gpioWrite(RED_LED, 1);
-    usleep(62500);
-    gpioWrite(RED_LED, 0);
-    usleep(62500);
+    while (arrToggleKillThread[1])
+    {
+        printf("RED BLINKING 8HZ \n");
+        gpioWrite(RED_LED, 1);
+        usleep(62500);
+        gpioWrite(RED_LED, 0);
+        usleep(62500);
+        if (arrToggleKillThread[1] == 0) // checks for toggle continiously in while loop, once killThread() is called, exit loop.
+                                         // Ensures thread properly terminates itself with no memory leaks
+        {
+            arrToggleKillThread[1] = 1; // resets toggle
+            break;
+        }
+    }
 }
 
 void *blink_green_2Hz_reduced_brightness()
 {
-    gpioPWM(GREEN_LED, 63);
-    usleep(250000);
-    gpioWrite(GREEN_LED, 0);
-    usleep(250000);
+    while (arrToggleKillThread[2])
+    {
+        printf(". . . . . GREEN BLINKING 2HZ \n");
+        gpioPWM(GREEN_LED, 63);
+        usleep(250000);
+        gpioWrite(GREEN_LED, 0);
+        usleep(250000);
+        if (arrToggleKillThread[2] == 0) // checks for toggle continiously in while loop, once killThread() is called, exit loop.
+                                         // Ensures thread properly terminates itself with no memory leaks
+        {
+            arrToggleKillThread[2] = 1; // resets toggle
+            break;
+        }
+    }
 }
